54b684b29493c95e93f9252566ddb5ca5e3da245
Abdul BOURAe <abdul.boura@gmail.com>
2024-09-14 16:52:57 +0200

    uptaded manually database contenent

--- tracker/management/commands/populate_food_db.py
+++ tracker/management/commands/populate_food_db.py
@@ -10,6 +10,12 @@ class Command(BaseCommand):
             {'name': 'Banana', 'calories_per_100g': 89, 'is_liquid': False},
             {'name': 'Chicken Breast', 'calories_per_100g': 165, 'is_liquid': False},
             {'name': 'Whole Milk', 'calories_per_100g': 61, 'is_liquid': True},
+            {'name': 'Egg', 'calories_per_100g': 155, 'is_liquid': False},
+            {'name': 'Salmon', 'calories_per_100g': 206, 'is_liquid': False},
+            {'name': 'Quinoa', 'calories_per_100g': 120, 'is_liquid': False},
+            {'name': 'Haddock (Eglefin)', 'calories_per_100g': 90, 'is_liquid': False},
+            {'name': 'Tofu', 'calories_per_100g': 76, 'is_liquid': False},
+            {'name': 'Greek Yogurt', 'calories_per_100g': 59, 'is_liquid': False},
             # Add more foods as needed
         ]
 
@@ -17,3 +23,9 @@ class Command(BaseCommand):
             Food.objects.create(**food_data)
 
         self.stdout.write(self.style.SUCCESS('Successfully populated food database'))
+
+        for food_data in foods:
+            Food.objects.create(**food_data)
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated food database'))
+
