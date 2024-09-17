159f54fac153f3974c3c367c23df97a2bf7f72c7
Abdul BOURAe <abdul.boura@gmail.com>
2024-09-17 14:49:11 +0200

    add the fonctionaly of reset password, still sent the message in my local console

--- calorie_tracker/settings.py
+++ calorie_tracker/settings.py
@@ -131,5 +131,12 @@ LOGOUT_REDIRECT_URL = 'tracker:home'
 
 AUTH_USER_MODEL = 'tracker.CustomUser'
 
-# For email backend (during development)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # For development
+# For production, you'd use something like:
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.your-email-provider.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'your-email@example.com'
+# EMAIL_HOST_PASSWORD = 'your-email-password'
--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# calorie_tracker/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -24,4 +26,8 @@ urlpatterns = [
     path('', include(('tracker.urls', 'tracker'), namespace='tracker')),
     path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='tracker:home'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='tracker/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='tracker/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='tracker/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='tracker/password_reset_complete.html'), name='password_reset_complete'),
 ]
