f5e1f859903dbbe8de8912901788fbd0c7b39994
Abdul BOURAe <abdul.boura@gmail.com>
2024-09-19 22:40:31 +0200

    write test for intakemodel, caloriesobjective. Error raise for intakemodel

--- tracker/tests.py
+++ tracker/tests.py
@@ -2,6 +2,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import CalorieObjective
+from tracker.models import Food, Intake, CalorieObjective
 
 class CustomUserModelTest(TestCase):
     def setUp(self):
@@ -42,3 +43,48 @@ class CustomUserModelTest(TestCase):
 
 
 
+class FoodModelTest(TestCase):
+    def test_create_food(self):
+        food = Food.objects.create(
+            name='Apple',
+            calories_per_100g=52,
+            carbohydrates_per_100g=14,
+            fats_per_100g=0.2,
+            proteins_per_100g=0.3,
+            is_liquid=False
+        )
+        self.assertEqual(food.name, 'Apple')
+        self.assertEqual(food.calories_per_100g, 52)
+
+class IntakeModelTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpass123')
+        self.food = Food.objects.create(name='Apple', calories_per_100g=52)
+
+    def test_create_intake(self):
+        intake = Intake.objects.create(
+            user=self.user,
+            food=self.food,
+            quantity=150
+        )
+        self.assertEqual(intake.user, self.user)
+        self.assertEqual(intake.food, self.food)
+        self.assertEqual(intake.quantity, 150)
+
+    def test_calculate_calories(self):
+        intake = Intake.objects.create(
+            user=self.user,
+            food=self.food,
+            quantity=150
+        )
+        self.assertAlmostEqual(intake.calculate_calories(), 78, places=2)
+
+class CalorieObjectiveModelTest(TestCase):
+    def test_create_calorie_objective(self):
+        objective = CalorieObjective.objects.create(
+            gender='F',
+            daily_calories=2000
+        )
+        self.assertEqual(objective.gender, 'F')
+        self.assertEqual(objective.daily_calories, 2000)
