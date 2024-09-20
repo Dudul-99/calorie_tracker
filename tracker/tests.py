7b71511937d8873dc9cb85b44d35ce086836ce8d
Abdul BOURAe <abdul.boura@gmail.com>
2024-09-20 20:39:06 +0200

    Form test validé

--- tracker/tests.py
+++ tracker/tests.py
@@ -3,6 +3,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from tracker.models import Food, Intake, CalorieObjective
+from tracker.forms import UserRegistrationForm, UserProfileForm, FoodForm, IntakeForm
 
 
 class CustomUserModelTest(TestCase):
@@ -66,7 +67,6 @@ class IntakeModelTest(TestCase):
                                         carbohydrates_per_100g=14, fats_per_100g=0.2, proteins_per_100g=0.3,
                                         is_liquid=False)
 
-
     def test_create_intake(self):
         intake = Intake.objects.create(
             user=self.user,
@@ -85,6 +85,7 @@ class IntakeModelTest(TestCase):
         )
         self.assertAlmostEqual(intake.calculate_calories(), 78, places=2)
 
+
 class CalorieObjectiveModelTest(TestCase):
     def test_create_calorie_objective(self):
         objective = CalorieObjective.objects.create(
@@ -93,3 +94,61 @@ class CalorieObjectiveModelTest(TestCase):
         )
         self.assertEqual(objective.gender, 'F')
         self.assertEqual(objective.daily_calories, 2000)
+
+
+
+# Forms tests
+class UserRegistrationFormTest(TestCase):
+    def test_user_registration_form_valid_data(self):
+        form = UserRegistrationForm(data={
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password1': 'testpass123',
+            'password2': 'testpass123',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_user_registration_form_invalid_data(self):
+        form = UserRegistrationForm(data={
+            'username': '',
+            'email': 'invalid_email',
+            'password1': 'short',
+            'password2': 'short',
+        })
+        self.assertFalse(form.is_valid())
+
+class UserProfileFormTest(TestCase):
+    def test_user_profile_form_valid_data(self):
+        form = UserProfileForm(data={
+            'age': 25,
+            'height': 180,
+            'weight': 75,
+            'gender': 'M',
+            'personal_calorie_objective': 2500,
+        })
+        self.assertTrue(form.is_valid())
+
+class FoodFormTest(TestCase):
+    def test_food_form_valid_data(self):
+        form = FoodForm(data={
+            'name': 'Banana',
+            'calories_per_100g': 89,
+            'carbohydrates_per_100g': 23,
+            'fats_per_100g': 0.3,
+            'proteins_per_100g': 1.1,
+            'is_liquid': False,
+        })
+        self.assertTrue(form.is_valid())
+
+class IntakeFormTest(TestCase):
+    def setUp(self):
+        self.food = Food.objects.create(name='Apple', calories_per_100g=52, carbohydrates_per_100g=14,
+                                        fats_per_100g=0.2, proteins_per_100g=0.3, is_liquid=False)
+
+    def test_intake_form_valid_data(self):
+        form = IntakeForm(data={
+            'food': self.food.id,
+            'quantity': 150,
+            'datetime': '2023-01-01 12:00:00',
+        })
+        self.assertTrue(form.is_valid())
